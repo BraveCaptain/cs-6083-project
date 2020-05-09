17004bd0b0eaf370787017c217a44def36627cd5
TangQianYong <qy_tang@yeah.net>
2020-05-08 22:26:42 -0400

    remove unrelated files

--- tex/scripts/g_detail.sql
+++
@@ -1,8 +0,0 @@
---data dictionary queries that details all tables, columns-dataype-size-mandatory/optional, and
---attribute comments of schema objects
-select table_name, column_name, data_type, data_precision, char_length, nullable, comments
-from user_tab_columns natural join user_col_comments
-
---data dictionary queries that details all constraints
-select table_name, constraint_name, constraint_type, search_condition, r_owner, r_constraint_name
-from user_constraints
